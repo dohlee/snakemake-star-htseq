a5e1c39a1eb6108f32ac46ada7331bb49eeaf36b
dohlee <dohlee.bioinfo@gmail.com>
2019-06-10 01:06:14 +0900

    Fetch test script from Github gist instead of having them in the repo.

---
+++ tests/test_reproducibility/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Test for reproducibility.
+se_result="02_htseq_count/se/test_single.htseq_count.result"
+se_result="02_htseq_count/se/test_paired.htseq_count.result"
+
+snakemake -s startup.smk -p
+snakemake -s ../../Snakefile --configfile config.yaml --config result_dir=result0 -p -j 2
+snakemake -s ../../Snakefile --configfile config.yaml --config result_dir=result1 -p -j 2
+
+se_a=$(md5sum result0/${se_result} | cut -d' ' -f1)
+se_b=$(md5sum result1/${se_result} | cut -d' ' -f1)
+pe_a=$(md5sum result0/${pe_result} | cut -d' ' -f1)
+pe_b=$(md5sum result1/${pe_result} | cut -d' ' -f1)
+
+if [ "$se_a" = "$se_b" ] && [ "pe_a" && "pe_b" ]; then
+    curl https://gist.githubusercontent.com/dohlee/3ea154d52932b27d042566605a2cb9e2/raw/update_reproducibility.sh -H 'Cache-control: no-cache' | bash /dev/stdin -n
+else
+    curl https://gist.githubusercontent.com/dohlee/3ea154d52932b27d042566605a2cb9e2/raw/update_reproducibility.sh -H 'Cache-control: no-cache' | bash /dev/stdin -y
+fi
+
+echo "Test exited with $?."
